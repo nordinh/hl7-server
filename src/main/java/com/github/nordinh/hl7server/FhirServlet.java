78c86ef9581b4265677e108b37d761afbed396c5
nordinh <nordin.haouari@gmail.com>
2018-11-14 11:14:40 +0100

    minor fixes

--- src/main/java/com/github/nordinh/hl7server/FhirServlet.java
+++ src/main/java/com/github/nordinh/hl7server/FhirServlet.java
@@ -3,7 +3,6 @@ package com.github.nordinh.hl7server;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.RestfulServer;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +12,8 @@ public class FhirServlet extends RestfulServer {
 
     private static final long serialVersionUID = 1L;
 
-    /**
-     * The initialize method is automatically called when the servlet is starting up, so it can
-     * be used to configure the servlet to define resource providers, or set up
-     * configuration, interceptors, etc.
-     */
     @Override
-    protected void initialize() throws ServletException {
+    protected void initialize() {
         List<IResourceProvider> resourceProviders = new ArrayList<>();
         setResourceProviders(resourceProviders);
     }
