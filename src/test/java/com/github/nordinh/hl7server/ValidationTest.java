bea8b31c1fadc94832b080ee4b2fb64e58a36bd9
nordinh <nordin.haouari@gmail.com>
2018-11-15 10:42:14 +0100

    Refactored test

--- src/test/java/com/github/nordinh/hl7server/ValidationTest.java
+++ src/test/java/com/github/nordinh/hl7server/ValidationTest.java
@@ -4,45 +4,41 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.SingleValidationMessage;
 import ca.uhn.fhir.validation.ValidationResult;
+import org.apache.commons.io.IOUtils;
 import org.hl7.fhir.dstu3.hapi.validation.FhirInstanceValidator;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ValidationTest {
 
-    @Test
-    public void testValidation() {
+    private FhirValidator validator;
+
+    @Before
+    public void setup() {
         FhirContext ctx = FhirContext.forDstu3();
 
-        FhirValidator validator = ctx.newValidator();
+        validator = ctx.newValidator();
         FhirInstanceValidator instanceValidator = new FhirInstanceValidator(new TestProfileValidationSupport());
         validator.registerValidatorModule(instanceValidator);
 
         instanceValidator.setAnyExtensionsAllowed(true);
+    }
 
-        String patient = "<Patient xmlns=\"http://hl7.org/fhir\">\n" +
-                "    <meta>\n" +
-                "        <profile value=\"http://healthdata.be/fhir/StructureDefinition/hd-patient\"/>\n" +
-                "    </meta>" +
-                "    <identifier>\n" +
-                "        <system value=\"http://healthdata.be/ssin\"></system>\n" +
-                "        <value value=\"12345678901\"></value>\n" +
-                "    </identifier>\n" +
-                "    <name>\n" +
-                "        <family value=\"Test\"></family>\n" +
-                "        <given value=\"PatientOne\"></given>\n" +
-                "    </name>\n" +
-                "    <gender value=\"female\"></gender>\n" +
-                "</Patient>";
+    @Test
+    public void testValidation() throws Exception {
+        String patient = IOUtils.resourceToString("/patient-1.xml", StandardCharsets.UTF_8);
 
         ValidationResult result = validator.validateWithResult(patient);
-
-        assertThat(result.isSuccessful(), is(true));
 
         for (SingleValidationMessage next : result.getMessages()) {
             System.out.println(" Next issue " + next.getSeverity() + " - " + next.getLocationString() + " - " + next.getMessage());
         }
+
+        assertThat(result.isSuccessful(), is(true));
     }
 }
